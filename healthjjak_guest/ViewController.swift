1a89dbb720d9d7a95d0c6fcdb203a0102aa91e42
popotan <popotan90@gmail.com>
2016-04-14 15:41:24 +0900

    Initial Commit

---
+++ healthjjak_guest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  healthjjak_guest
+//
+//  Created by 이가람 on 2016. 4. 14..
+//  Copyright © 2016년 이가람. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ healthjjak_guestTests/healthjjak_guestTests.swift
@@ -0,0 +1,36 @@
+//
+//  healthjjak_guestTests.swift
+//  healthjjak_guestTests
+//
+//  Created by 이가람 on 2016. 4. 14..
+//  Copyright © 2016년 이가람. All rights reserved.
+//
+
+import XCTest
+@testable import healthjjak_guest
+
+class healthjjak_guestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
